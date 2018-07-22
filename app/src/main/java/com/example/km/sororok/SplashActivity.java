1edf9f3739062064c1fbb256a4ec100b0065703c
Hanbin Ju <robertnori8@gmail.com>
2018-07-22 19:21:28 +0900

    스플래시 화면 및 메인 액티비티 세팅

---
+++ app/src/main/java/com/example/km/sororok/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.km.sororok;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, 2000);
+    }
+}
