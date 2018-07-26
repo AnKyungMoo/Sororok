792da494c0803a8843ed36aacafd686c1fb19590
munjung <jekang0123@gmail.com>
2018-07-27 01:47:35 +0900

    UserGalleryActivity 추가

--- app/src/main/java/com/example/km/sororok/activity/UserGalleryActivity.java
+++ app/src/main/java/com/example/km/sororok/activity/UserGalleryActivity.java
@@ -1,15 +1,140 @@
 package com.example.km.sororok.activity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
+import android.widget.Toast;
 
 import com.example.km.sororok.R;
+import com.example.km.sororok.adapter.GalleryAdapter;
+
+import java.util.ArrayList;
 
 public class UserGalleryActivity extends AppCompatActivity {
+
+    private static GridView gridGallery;
+    private static GalleryAdapter galleryAdapter;
+    private static ArrayList<String> galleryImageUrls;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acrivity_user_gallery);
+
+        Intent intent = getIntent();
+
+        if (checkPermissionREAD_EXTERNAL_STORAGE(this)) {
+            initComponent();
+            fetchGalleryImages();
+            setUpGridView();
+        }
+
+
     }
+
+    private void fetchGalleryImages() {
+
+        final String[] columns = {MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID};
+        final String orderBy = MediaStore.Images.Media.DATE_TAKEN;//order data by date
+        Cursor imgCursor = managedQuery(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, null,
+                null, orderBy + " DESC");
+
+        galleryImageUrls = new ArrayList<>();//Init array
+
+        for (int i = 0; i < imgCursor.getCount(); i++) {
+            imgCursor.moveToPosition(i);
+            int dataColumnIndex = imgCursor.getColumnIndex(MediaStore.Images.Media.DATA);//get column index
+             galleryImageUrls.add(imgCursor.getString(dataColumnIndex));//get Image from column index
+             System.out.println("Array path : " + galleryImageUrls.get(i));
+        }
+    }
+
+    private void setUpGridView() {
+        galleryAdapter = new GalleryAdapter(UserGalleryActivity.this, galleryImageUrls);
+        gridGallery.setAdapter(galleryAdapter);
+    }
+
+    public void initComponent(){
+        gridGallery = (GridView)findViewById(R.id.grid_user_gallery);
+    }
+
+
+    //일정 수준의 버전 확인
+    public boolean checkPermissionREAD_EXTERNAL_STORAGE(
+            final Context context) {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        (Activity) context,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    showDialog("External storage", context,
+                            Manifest.permission.READ_EXTERNAL_STORAGE);
+
+                } else {
+                    ActivityCompat
+                            .requestPermissions(
+                                    (Activity) context,
+                                    new String[] { Manifest.permission.READ_EXTERNAL_STORAGE },
+                                    100);
+                }
+                return false;
+            } else {
+                return true;
+            }
+
+        } else {
+            return true;
+        }
+    }
+
+    public void showDialog(final String msg, final Context context,
+                           final String permission) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle("Permission necessary");
+        alertBuilder.setMessage(msg + " permission is necessary");
+        alertBuilder.setPositiveButton(android.R.string.yes,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions((Activity) context,
+                                new String[] { permission },
+                                100);
+                    }
+                });
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 100:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // do your stuff
+                } else {
+                    Toast.makeText(this, "permission not granted", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/km/sororok/adapter/GalleryAdapter.java
@@ -0,0 +1,76 @@
+package com.example.km.sororok.adapter;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import com.example.km.sororok.R;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import java.util.ArrayList;
+
+public class GalleryAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<String> imageUrls;
+    private SparseBooleanArray mSparseBooleanArray;//Variable to store selected Images
+    private DisplayImageOptions options;
+
+    public GalleryAdapter(Context context, ArrayList<String> imageUrls) {
+        this.context = context;
+        this.imageUrls = imageUrls;
+        mSparseBooleanArray = new SparseBooleanArray();
+        options = new DisplayImageOptions.Builder()
+                .cacheInMemory(true)
+                .resetViewBeforeLoading(true).cacheOnDisk(true)
+                .considerExifParams(true).bitmapConfig(Bitmap.Config.RGB_565)
+                .build();
+    }
+
+    //Method to return selected Images
+    public ArrayList<String> getCheckedItems() {
+        ArrayList<String> mTempArry = new ArrayList<>();
+
+        for (int i = 0; i < imageUrls.size(); i++) {
+            if (mSparseBooleanArray.get(i)) {
+                mTempArry.add(imageUrls.get(i));
+            }
+        }
+        return mTempArry;
+    }
+
+    @Override
+    public int getCount() {
+        return imageUrls.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return imageUrls.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (view == null)
+            view = inflater.inflate(R.layout.gridview_item, viewGroup, false);//Inflate layout
+
+        final ImageView imageView = (ImageView) view.findViewById(R.id.img_user_gallery);
+
+        ImageLoader.getInstance().init(ImageLoaderConfiguration.createDefault(this.context)); //추가
+        ImageLoader.getInstance().displayImage("file://" + imageUrls.get(position), imageView, options);
+
+        return view;
+    }
+}
