68849f314eda37d1cc469e48e01d46fa9212ec47
munjung <jekang0123@gmail.com>
2018-07-26 18:25:11 +0900

    그룹 추가 화면 생성!

---
+++ app/src/main/java/com/example/km/sororok/activity/NewGroupActivity.java
@@ -0,0 +1,16 @@
+package com.example.km.sororok.activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import com.example.km.sororok.R;
+
+
+//그룹 생성 화면
+public class NewGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_group);
+    }
+}
