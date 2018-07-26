4732def187bc64b8d46d3e23bcbaeae2eb46833f
munjung <jekang0123@gmail.com>
2018-07-27 00:59:07 +0900

    갤러리 레이아웃 추가

--- app/src/main/java/com/example/km/sororok/activity/MyPageActivity.java
+++ app/src/main/java/com/example/km/sororok/activity/MyPageActivity.java
@@ -1,18 +1,41 @@
 package com.example.km.sororok.activity;
 import com.example.km.sororok.R;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MyPageActivity extends AppCompatActivity {
 
-    ImageView imgUserPhoto;
+    private static final int REQUEST_USER_GALLERY = 1;
+
+    private ImageView imgUserPhoto;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
+        initComponent();
+
+        imgUserPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MyPageActivity.this, UserGalleryActivity.class);
+                startActivityForResult(i, REQUEST_USER_GALLERY);
+
+            }
+        });
+    }
+
+    public void initComponent(){
         imgUserPhoto = (ImageView)findViewById(R.id.img_user_photo);
+    }
+
+    @Override
+    public void startActivityForResult(Intent intent, int requestCode) {
+        super.startActivityForResult(intent, requestCode);
     }
 }
---
+++ app/src/main/java/com/example/km/sororok/activity/UserGalleryActivity.java
@@ -0,0 +1,15 @@
+package com.example.km.sororok.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.km.sororok.R;
+
+public class UserGalleryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acrivity_user_gallery);
+    }
+}
