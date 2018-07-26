179efe1631330f652bb5b8de3a0644a59a1de839
munjung <jekang0123@gmail.com>
2018-07-26 19:15:20 +0900

    마이페이지 추가

---
+++ app/src/main/java/com/example/km/sororok/activity/MyPageActivity.java
@@ -0,0 +1,13 @@
+package com.example.km.sororok.activity;
+import com.example.km.sororok.R;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MyPageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_page);
+    }
+}
