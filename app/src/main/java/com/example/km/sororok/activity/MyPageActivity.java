588bef7c0990b44d700db8ac3e36a5737c7e38bc
munjung <jekang0123@gmail.com>
2018-07-26 23:16:40 +0900

    adpater 패키지 추가

--- app/src/main/java/com/example/km/sororok/activity/MyPageActivity.java
+++ app/src/main/java/com/example/km/sororok/activity/MyPageActivity.java
@@ -3,11 +3,16 @@ import com.example.km.sororok.R;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MyPageActivity extends AppCompatActivity {
+
+    ImageView imgUserPhoto;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
+        imgUserPhoto = (ImageView)findViewById(R.id.img_user_photo);
     }
 }
