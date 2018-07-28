abe9982c9aecbf66dba59e2de23fac274c065fa6
munjung <jekang0123@gmail.com>
2018-07-28 17:22:49 +0900

    그룹 생성 화면 건드는중

--- app/src/main/java/com/example/km/sororok/activity/NewGroupActivity.java
+++ app/src/main/java/com/example/km/sororok/activity/NewGroupActivity.java
@@ -1,10 +1,17 @@
 package com.example.km.sororok.activity;
+
 import android.content.Intent;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 
 import com.example.km.sororok.R;
 
@@ -14,6 +21,10 @@ public class NewGroupActivity extends AppCompatActivity {
 
     private static final int REQUEST_USER_GALLERY = 1;
     private ImageView imgGroupPhoto;
+    private EditText groupName, groupExplain;
+    private Button completeBtn;
+    private SeekBar totalMemberSeek;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,9 +38,37 @@ public class NewGroupActivity extends AppCompatActivity {
                 startActivityForResult(i, REQUEST_USER_GALLERY);
             }
         });
+        seekListener();
     }
 
     public void initComponent(){
         imgGroupPhoto = (ImageView)findViewById(R.id.img_new_group);
+        groupName = (EditText)findViewById(R.id.edit_group_name);
+        groupExplain = (EditText)findViewById(R.id.edit_group_explain);
+        completeBtn = (Button)findViewById(R.id.btn_new_group_complete);
+        totalMemberSeek = (SeekBar)findViewById(R.id.seek_total_member);
+        imgGroupPhoto.setBackground(new ShapeDrawable(new OvalShape()));
+        if(Build.VERSION.SDK_INT >= 21) {
+            imgGroupPhoto.setClipToOutline(true);
+        }
+    }
+
+    public void seekListener(){
+        totalMemberSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
