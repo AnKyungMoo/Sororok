710f59055f3f87165c167eb0662291ee01321cf0
KM <sodp2@naver.com>
2018-08-01 23:34:57 +0900

    MainActivity에서 프로필 클릭하면 NewGroupActivity로 이동하는 버튼 추가

--- app/src/main/java/com/example/km/sororok/activity/MainActivity.java
+++ app/src/main/java/com/example/km/sororok/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.km.sororok.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.km.sororok.R;
 
@@ -10,9 +13,22 @@ import com.example.km.sororok.R;
  */
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton profileButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profileButton = (ImageButton) findViewById(R.id.profileButton);
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newGroupIntent = new Intent(MainActivity.this, NewGroupActivity.class);
+                startActivity(newGroupIntent);
+                finish();
+            }
+        });
     }
 }
