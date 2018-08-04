2f79729a4796fd14cb66de0d049c05ea1d177fdf
munjung <jekang0123@gmail.com>
2018-08-04 16:13:40 +0900

    시작한다 드로어

--- app/src/main/java/com/example/km/sororok/activity/MainActivity.java
+++ app/src/main/java/com/example/km/sororok/activity/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.km.sororok.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.example.km.sororok.R;
@@ -14,12 +15,20 @@ import com.example.km.sororok.R;
 public class MainActivity extends AppCompatActivity {
 
     ImageButton profileButton;
+    private Button historyBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        historyBtn = (Button)findViewById(R.id.btn_history);
+        historyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), DrawerActivity.class);
+                startActivity(i);
+            }
+        });
         profileButton = (ImageButton) findViewById(R.id.profileButton);
 
         profileButton.setOnClickListener(new View.OnClickListener() {
