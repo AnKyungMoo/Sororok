98587e82539c22f1f68860ffd4e6c6807dabd1bc
munjung <jekang0123@gmail.com>
2018-08-04 23:24:34 +0900

    히스토리 레이아웃 연결

--- app/src/main/java/com/example/km/sororok/activity/MainActivity.java
+++ app/src/main/java/com/example/km/sororok/activity/MainActivity.java
@@ -2,12 +2,11 @@ package com.example.km.sororok.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.ListView;
 
 import com.example.km.sororok.R;
 
@@ -16,52 +15,40 @@ import com.example.km.sororok.R;
  */
 public class MainActivity extends AppCompatActivity{
 
-    ImageButton profileButton;
+    private Button profileButton;
     private Button historyBtn;
+    private DrawerLayout drawerLayout;
+    private ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        historyBtn = (Button)findViewById(R.id.btn_history);
+        initComponent();
+
+
         historyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*Intent i = new Intent(getApplicationContext(), DrawerActivity.class);
-                startActivity(i);*/
+                drawerLayout.openDrawer(listView);
             }
         });
-        profileButton = (ImageButton) findViewById(R.id.profileButton);
-
         profileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent newGroupIntent = new Intent(MainActivity.this, DrawerActivity.class);
+                Intent newGroupIntent = new Intent(MainActivity.this, NewGroupActivity.class);
                 startActivityForResult(newGroupIntent,100);
             }
         });
 
-
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);*/
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+    public void initComponent(){
+        drawerLayout = (DrawerLayout)findViewById(R.id.layout_drawer);
+        listView = (ListView)findViewById(R.id.list_drawer);
+        historyBtn = (Button)findViewById(R.id.btn_history);
+        profileButton = (Button) findViewById(R.id.profileButton);
     }
+
 }
--- app/src/main/java/com/example/km/sororok/activity/SplashActivity.java
+++ app/src/main/java/com/example/km/sororok/activity/SplashActivity.java
@@ -35,7 +35,7 @@ public class SplashActivity extends AppCompatActivity {
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    Intent mainIntent = new Intent(SplashActivity.this, MyPageActivity.class);
+                    Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
                     SplashActivity.this.startActivity(mainIntent);
                     SplashActivity.this.finish();
                 }
