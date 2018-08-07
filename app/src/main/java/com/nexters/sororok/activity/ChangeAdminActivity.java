047cb2b68c9a2aa7b9f3ae0d71fbf9e255a5e5ac
munjung <jekang0123@gmail.com>
2018-08-07 23:20:04 +0900

    관리자 변경 레이아웃 버튼 추가

--- app/src/main/java/com/nexters/sororok/activity/ChangeAdminActivity.java
+++ app/src/main/java/com/nexters/sororok/activity/ChangeAdminActivity.java
@@ -3,13 +3,30 @@ package com.nexters.sororok.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.nexters.sororok.R;
 
 public class ChangeAdminActivity extends AppCompatActivity {
+
+    private Button updateBtn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_admin);
+
+        initComponent();
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    public void initComponent(){
+        updateBtn = findViewById(R.id.btn_admin_update);
     }
 }
